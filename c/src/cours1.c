29380d003936c4f7106451cf42be859c584bee18
yaco <tde.airsoftliveaction@gmail.com>
2018-12-12 00:45:00 +0100

    ajout fonction C

--- c/src/cours1.c
+++ c/src/cours1.c
@@ -1,1 +1,46 @@
 #include "cours1.h"
+#include <stdio.h>
+
+void print_arg(int nb_arg, char *argv[]){
+
+
+  for (int i = 0; i < nb_arg ; i++)
+  {
+    printf("argument %d : %s \n", i, argv[i]);
+  }
+
+}
+
+void print_int(char *n){
+
+  printf("entier = %s \n", n);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+print_arg(argc,argv);
+print_int("6");
+int a = 0;
+a = 1;
+
+return a;
+}
+
+
+
+
