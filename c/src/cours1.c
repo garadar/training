95ec68be67395e55f2b5056382cf16c470f468d3
Adrien Albert <adrien.albert@epita.fr>
2018-12-11 23:02:18 +0100

    print_arg

--- c/src/cours1.c
+++ c/src/cours1.c
@@ -1,1 +1,26 @@
 #include "cours1.h"
+#include <stdio.h>
+
+
+
+void print_arg(int nb_arg, char *argv[]){
+
+
+  for (int i = 0; i < nb_arg - 1; i++)
+  {
+    printf("argument %d : %s \n", i+1, argv[i+1]);
+  } 
+
+
+}
+
+
+
+int main(int argc, char *argv[])
+{
+
+//printf("argc = %d\n",argc);
+print_arg(argc,argv);
+
+return 0;
+}
