77e718320d4f3a28a1f788fb555254191afe9b3c
yaco <tde.airsoftliveaction@gmail.com>
2018-12-23 03:22:05 +0100

    cours pour fonctions, a terminer

--- c/src/cours1.c
+++ c/src/cours1.c
@@ -1,5 +1,6 @@
 #include "cours1.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
 
@@ -14,13 +15,27 @@ void print_arg(int nb_arg, char *argv[]){
 
 }
 
+void print_int(int n){
+
+  printf("n = %d\n", n);
+}
+	
+
+int sum(int a, int b){
+
+return a + b;
+}
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
 
+
+int x = atoi(argv[1]);
 //printf("argc = %d\n",argc);
 print_arg(argc,argv);
+print_int(x);
+int res = sum(x,atoi(argv[2]));
+print_int(res);
 
 return 0;
 }
