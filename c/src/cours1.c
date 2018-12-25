7a51d36718e56f6564137885cb009eb3eaf49f8f
yaco <tde.airsoftliveaction@gmail.com>
2018-12-25 14:43:43 +0100

    ajout fonctions

--- c/src/cours1.c
+++ c/src/cours1.c
@@ -15,6 +15,7 @@ void print_arg(int nb_arg, char *argv[]){
 
 }
 
+
 void print_int(int n){
 
   printf("n = %d\n", n);
@@ -23,19 +24,42 @@ void print_int(int n){
 
 int sum(int a, int b){
 
-return a + b;
+  return a + b;
 }
 
+
+int power(int a, int b){
+
+  return a ^ b;	
+}
+
+
+void print_char(char c){
+
+  printf("c = %c\n", c);
+}
+
+
+void print_string(char *s){
+
+  printf("string = %d\n", *s);
+}
+ 
 
 int main(int argc, char *argv[]){
 
 
 int x = atoi(argv[1]);
+char y = atoi(argv[1]);
+char *s = atoi(*argv[1]);
 //printf("argc = %d\n",argc);
 print_arg(argc,argv);
 print_int(x);
 int res = sum(x,atoi(argv[2]));
+//int res = power(x,atoi(argv[2]));(j'ai mis // en attendant une solution)
 print_int(res);
+print_char(y);
+print_string(*s);
 
 return 0;
 }
